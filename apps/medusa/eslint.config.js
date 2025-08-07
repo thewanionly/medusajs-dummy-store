619a181791ba9c3b6fb13aacd8f9289ad46b39d6
Elloani Ross Pitogo <pelloani@gmail.com>
2025-08-07 19:54:56 +0800

    chore: ensure correct usage of eslint config

--- apps/medusa/eslint.config.js
+++ apps/medusa/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from '@repo/eslint-config/base';
+import { config } from '@mds/eslint-config/base';
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default config;
