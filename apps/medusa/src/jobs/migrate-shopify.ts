9aafe6525a63ad7cc474844918d672cff88bfa55
Elloani Ross Pitogo <pelloani@gmail.com>
2025-08-21 00:30:13 +0800

    feat: create migration logic for shopify product data feed

---
+++ apps/medusa/src/jobs/migrate-shopify.ts
@@ -0,0 +1,33 @@
+import { MedusaContainer } from '@medusajs/framework/types';
+
+import { migrateProductsFromShopifyWorkflow } from '../workflows/migrate-products-from-shopify';
+
+export default async function migrateShopifyJob(container: MedusaContainer) {
+  const logger = container.resolve('logger');
+  logger.info('Migrating products from Shopify...');
+
+  let currentPage = 0;
+  const pageSize = 10;
+  const totalCount = 30; // only migrate 30 products for now
+
+  do {
+    currentPage++;
+
+    const { result: pagination } = await migrateProductsFromShopifyWorkflow(
+      container
+    ).run({
+      input: {
+        page: currentPage,
+        limit: pageSize,
+      },
+    });
+  } while (currentPage * pageSize < totalCount);
+
+  logger.info('Finished migrating products from Shopify');
+}
+
+export const config = {
+  name: 'migrate-shopify-job',
+  schedule: '0 0 * * *',
+  // schedule: '* * * * *', // runs every minute for testing purposes
+};
