d95cd066752a73e83616b7cb026f8716ce1e54e5
Elloani Ross Pitogo <pelloani@gmail.com>
2025-08-25 18:10:47 +0800

    feat: add support for pagination params by adding validateAndTransformQuery

--- apps/medusa/src/api/store/products-with-custom/route.ts
+++ apps/medusa/src/api/store/products-with-custom/route.ts
@@ -1,7 +1,24 @@
+import z from 'zod';
+
 import { MedusaRequest, MedusaResponse } from '@medusajs/framework/http';
 import { ContainerRegistrationKeys } from '@medusajs/framework/utils';
+import { createFindParams } from '@medusajs/medusa/api/utils/validators';
 
 import ProductProductExtendedLink from '../../../links/product-productExtended';
+
+// Define  custom filterable fields in a separate schema
+const CustomProductFilters = z.object({
+  id: z.union([z.string(), z.array(z.string())]).optional(),
+  vendor: z.string().optional(),
+  title: z.string().optional(),
+});
+
+// Create the base schema with standard list parameters
+const BaseFindParams = createFindParams();
+
+// Merge the two schemas together
+export const GetProductsWithCustomSchema =
+  BaseFindParams.merge(CustomProductFilters);
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
@@ -38,25 +55,56 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
     );
 
     // Step 3: Get all products that have the specified productIds
-    const { data: products } = await query.graph({
-      entity: 'product',
-      fields: ['*', 'product_extended.*'],
-      filters: { id: productIds },
-    });
+    const { data: products, metadata: { count, take, skip } = {} } =
+      await query.graph({
+        entity: 'product',
+        ...req.queryConfig,
+        fields:
+          req.queryConfig.fields.length > 0
+            ? [...req.queryConfig.fields, 'id']
+            : [
+                '*',
+                'options.*',
+                'tags.*',
+                'images.*',
+                'variants.*',
+                'sales_channels.*',
+                'product_extended.*',
+              ],
+        filters: { id: productIds },
+      });
 
     res.json({
       products,
+      count,
+      limit: take,
+      offset: skip,
     });
 
     return;
   }
 
-  const { data: products } = await query.graph({
-    entity: 'product',
-    fields: ['*'],
-  });
+  const { data: products, metadata: { count, take, skip } = {} } =
+    await query.graph({
+      entity: 'product',
+      ...req.queryConfig,
+      fields:
+        req.queryConfig.fields.length > 0
+          ? [...req.queryConfig.fields, 'id']
+          : [
+              '*',
+              'options.*',
+              'tags.*',
+              'images.*',
+              'variants.*',
+              'sales_channels.*',
+            ],
+    });
 
   res.json({
     products,
+    count,
+    limit: take,
+    offset: skip,
   });
 };
