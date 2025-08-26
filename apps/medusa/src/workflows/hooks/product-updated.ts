26f1499b6072b4f814dab9ccb1d0bdddab4c407d
Elloani Ross Pitogo <pelloani@gmail.com>
2025-08-26 23:16:21 +0800

    feat: add custom property to product_variant_extended when products are updated

--- apps/medusa/src/workflows/hooks/product-updated.ts
+++ apps/medusa/src/workflows/hooks/product-updated.ts
@@ -4,18 +4,24 @@ import {
   UpdateProductExtendedFromProductStepInput,
   updateProductExtendedFromProductWorkflow,
 } from '../update-product-extended-from-product';
+import {
+  UpdateProductVariantExtendedFromProductStepInput,
+  updateProductVariantExtendedFromProductWorkflow,
+} from '../update-product-variant-extended-from-product-variant';
 
 updateProductsWorkflow.hooks.productsUpdated(
   async ({ products, additional_data }, { container }) => {
     const workflow = updateProductExtendedFromProductWorkflow(container);
+    const variantWorkflow =
+      updateProductVariantExtendedFromProductWorkflow(container);
 
-    console.log('### productsUpdated hook additional_data', additional_data);
+    console.log('### productsUpdated hook additional data', additional_data);
 
     for (let i = 0; i < products.length; i++) {
       const product = products[i];
 
-      const additionalDataValue = Array.isArray(additional_data)
-        ? additional_data?.[i]
+      const additionalDataValue = additional_data?.isFromMigration
+        ? (additional_data as { product: unknown[] })?.product?.[i]
         : additional_data;
 
       await workflow.run({
@@ -24,6 +30,22 @@ updateProductsWorkflow.hooks.productsUpdated(
           additional_data: additionalDataValue,
         } as UpdateProductExtendedFromProductStepInput,
       });
+
+      if (additional_data?.isFromMigration) {
+        for (let j = 0; j < product.variants.length; j++) {
+          const variant = product.variants[j];
+          const additionalVariantsData = (
+            additional_data as { variant: Record<string, any> }
+          ).variant[variant.id];
+
+          await variantWorkflow.run({
+            input: {
+              variant,
+              additional_data: additionalVariantsData,
+            } as UpdateProductVariantExtendedFromProductStepInput,
+          });
+        }
+      }
     }
   }
 );
