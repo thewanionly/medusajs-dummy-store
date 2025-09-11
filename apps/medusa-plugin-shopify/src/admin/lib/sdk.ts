5d69f13d87044a7e41ac3426881af8707241415d
Elloani Ross Pitogo <pelloani@gmail.com>
2025-09-11 17:28:20 +0800

    fix: use __BACKEND_URL__ when accessing the `backendUrl` set in medusa-config in apps/medusa

--- apps/medusa-plugin-shopify/src/admin/lib/sdk.ts
+++ apps/medusa-plugin-shopify/src/admin/lib/sdk.ts
@@ -1,14 +1,11 @@
 import Medusa from '@medusajs/js-sdk';
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = 'http://localhost:9000';
+const DEFAULT_MEDUSA_BACKEND_URL = 'http://localhost:9000';
 
-if (import.meta.env.MEDUSA_BACKEND_URL) {
-  MEDUSA_BACKEND_URL = import.meta.env.MEDUSA_BACKEND_URL;
-}
 
 export const sdk = new Medusa({
-  baseUrl: MEDUSA_BACKEND_URL,
+  baseUrl: __BACKEND_URL__ || DEFAULT_MEDUSA_BACKEND_URL,
   debug: import.meta.env.NODE_ENV === 'development',
   auth: {
     type: 'session',
--- apps/medusa-plugin-shopify/src/admin/vite-env.d.ts
+++ apps/medusa-plugin-shopify/src/admin/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+// Reference: https://docs.medusajs.com/learn/fundamentals/admin/environment-variables#environment-variables-in-plugins
+declare const __BACKEND_URL__: string
+declare const __BASE__: string
+declare const __STOREFRONT_URL__: string
