3ff740d1aec1d5ee9ff3860f2d6db2f33dd92a02
Elloani Ross A. Pitogo <pelloani@gmail.com>
2025-09-12 16:57:01 +0800

    fix: prepend `NEXT_PUBLIC_` to backend URL env variable in storefront (#19)

--- apps/medusa-storefront/src/lib/config.ts
+++ apps/medusa-storefront/src/lib/config.ts
@@ -6,14 +6,11 @@ import {
 import Medusa from '@medusajs/js-sdk';
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = 'http://localhost:9000';
-
-if (process.env.MEDUSA_BACKEND_URL) {
-  MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL;
-}
+const DEFAULT_MEDUSA_BACKEND_URL = 'http://localhost:9000';
 
 export const sdk = new Medusa({
-  baseUrl: MEDUSA_BACKEND_URL,
+  baseUrl:
+    process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || DEFAULT_MEDUSA_BACKEND_URL,
   debug: process.env.NODE_ENV === 'development',
   publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
 });
