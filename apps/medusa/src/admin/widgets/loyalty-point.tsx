a8abbf142fffb7538afe83486c87db8b5f4d7c6b
Riyana Gueco <rgueco@griddynamics.com>
2025-09-19 19:20:05 +0800

    feat(apps/medusa): implement loyalty points
    - add loyalty point widget to admin dashboard
    
    MDS-50

---
+++ apps/medusa/src/admin/widgets/loyalty-point.tsx
@@ -0,0 +1,36 @@
+import { defineWidgetConfig } from '@medusajs/admin-sdk';
+import { AdminCustomer, DetailWidgetProps } from '@medusajs/framework/types';
+import { Container, Heading, Text } from '@medusajs/ui';
+import { useQuery } from '@tanstack/react-query';
+
+import { sdk } from '../lib/sdk';
+
+const LoyaltyPointWidget = ({
+  data: customer,
+}: DetailWidgetProps<AdminCustomer>) => {
+  const { data, isLoading, error } = useQuery<{ points: number | null }>({
+    queryFn: () =>
+      sdk.client.fetch(`/admin/loyalty-points?customer_id=${customer.id}`),
+    queryKey: ['customer-loyalty-point', customer.id],
+  });
+
+  if (error) return null;
+
+  return (
+    <Container className="divide-y p-0">
+      <div className="flex items-center justify-between px-6 py-4">
+        <Heading level="h2">Other Information</Heading>
+      </div>
+      <div className="text-ui-fg-subtle grid grid-cols-2 items-center px-6 py-4">
+        <Text size="small">Loyalty Points</Text>
+        <Text size="small">{isLoading ? '...' : data?.points || 0}</Text>
+      </div>
+    </Container>
+  );
+};
+
+export const config = defineWidgetConfig({
+  zone: 'customer.details.side.after',
+});
+
+export default LoyaltyPointWidget;
---
+++ apps/medusa/src/workflows/loyalty/apply-loyalty-on-cart.ts
@@ -0,0 +1,177 @@
+import { CreatePromotionDTO } from '@medusajs/framework/types';
+import { PromotionActions } from '@medusajs/framework/utils';
+import {
+  WorkflowResponse,
+  createWorkflow,
+  transform,
+} from '@medusajs/framework/workflows-sdk';
+import {
+  createPromotionsStep,
+  updateCartPromotionsWorkflow,
+  updateCartsStep,
+  useQueryGraphStep,
+} from '@medusajs/medusa/core-flows';
+
+import {
+  CUSTOMER_ID_PROMOTION_RULE_ATTRIBUTE,
+  CartData,
+} from '../../utils/promo';
+import { getCartLoyaltyPromoStep } from './steps/get-cart-loyalty-promo';
+import {
+  GetCartLoyaltyPromoAmountStepInput,
+  getCartLoyaltyPromoAmountStep,
+} from './steps/get-cart-loyalty-promo-amount';
+import {
+  ValidateCustomerExistsStepInput,
+  validateCustomerExistsStep,
+} from './steps/validate-customer-exists';
+
+type WorkflowInput = {
+  cart_id: string;
+};
+
+const fields = [
+  'id',
+  'customer.*',
+  'promotions.*',
+  'promotions.application_method.*',
+  'promotions.rules.*',
+  'promotions.rules.values.*',
+  'currency_code',
+  'total',
+  'metadata',
+];
+
+const returnedFields = [
+  '*',
+  'customer.*',
+  'promotions.*',
+  'promotions.application_method.*',
+  'promotions.rules.*',
+  'promotions.rules.values.*',
+  'items.*',
+  'metadata.*',
+];
+
+export const applyLoyaltyOnCartWorkflow = createWorkflow(
+  'apply-loyalty-on-cart',
+  (input: WorkflowInput) => {
+    const { data: carts } = useQueryGraphStep({
+      entity: 'cart',
+      fields,
+      filters: {
+        id: input.cart_id,
+      },
+      options: {
+        throwIfKeyNotFound: true,
+      },
+    });
+
+    validateCustomerExistsStep({
+      customer: carts[0].customer,
+    } as ValidateCustomerExistsStepInput);
+
+    getCartLoyaltyPromoStep({
+      cart: carts[0] as unknown as CartData,
+      throwErrorOn: 'found',
+    });
+
+    const amount = getCartLoyaltyPromoAmountStep({
+      cart: carts[0],
+    } as unknown as GetCartLoyaltyPromoAmountStepInput);
+
+    const promoToCreate = transform(
+      {
+        carts,
+        amount,
+      },
+      (data) => {
+        const randomStr = Math.random().toString(36).substring(2, 8);
+        const uniqueId = (
+          'LOYALTY-' +
+          data.carts[0].customer?.first_name +
+          '-' +
+          randomStr
+        ).toUpperCase();
+        return {
+          code: uniqueId,
+          type: 'standard',
+          status: 'active',
+          application_method: {
+            type: 'fixed',
+            value: data.amount,
+            target_type: 'order',
+            currency_code: data.carts[0].currency_code,
+            allocation: 'across',
+          },
+          rules: [
+            {
+              attribute: CUSTOMER_ID_PROMOTION_RULE_ATTRIBUTE,
+              operator: 'eq',
+              values: [data.carts[0].customer!.id],
+            },
+          ],
+          campaign: {
+            name: uniqueId,
+            description:
+              'Loyalty points promotion for ' + data.carts[0].customer!.email,
+            campaign_identifier: uniqueId,
+            budget: {
+              type: 'usage',
+              limit: 1,
+            },
+          },
+        };
+      }
+    );
+
+    const loyaltyPromo = createPromotionsStep([
+      promoToCreate,
+    ] as CreatePromotionDTO[]);
+
+    const { metadata, ...updatePromoData } = transform(
+      {
+        carts,
+        promoToCreate,
+        loyaltyPromo,
+      },
+      (data) => {
+        const promos = [
+          ...((data.carts[0].promotions
+            ?.map((promo) => promo?.code)
+            .filter(Boolean) || []) as string[]),
+          data.promoToCreate.code,
+        ];
+
+        return {
+          cart_id: data.carts[0].id,
+          promo_codes: promos,
+          action: PromotionActions.ADD,
+          metadata: {
+            loyalty_promo_id: data.loyaltyPromo[0].id,
+          },
+        };
+      }
+    );
+
+    updateCartPromotionsWorkflow.runAsStep({
+      input: updatePromoData,
+    });
+
+    updateCartsStep([
+      {
+        id: input.cart_id,
+        metadata,
+      },
+    ]);
+
+    // Retrieve cart with updated promotions
+    const { data: updatedCarts } = useQueryGraphStep({
+      entity: 'cart',
+      fields: returnedFields,
+      filters: { id: input.cart_id },
+    }).config({ name: 'retrieve-cart' });
+
+    return new WorkflowResponse(updatedCarts[0]);
+  }
+);
