7068e0f128edc14e00eee220a8fc4a4c1fce2eb9
Elloani Ross Pitogo <pelloani@gmail.com>
2025-10-03 16:14:27 +0800

    feat: implement wishlist module

---
+++ apps/medusa/src/modules/wishlist/service.ts
@@ -0,0 +1,9 @@
+import { MedusaService } from '@medusajs/framework/utils';
+
+import { Wishlist } from './models/wishlist';
+import { WishlistItem } from './models/wishlist-item';
+
+export default class WishlistModuleService extends MedusaService({
+  Wishlist,
+  WishlistItem,
+}) {}
