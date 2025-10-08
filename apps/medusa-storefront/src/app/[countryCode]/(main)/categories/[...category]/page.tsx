e994036a5961c2771dc5f14ead7bc98c8a3ea49b
Elloani Ross Pitogo <pelloani@gmail.com>
2025-10-08 14:24:51 +0800

    fix: remove generateStaticParams
    This is to prevent generating pages at build time making API calls to BFF server which could possibly be down (due to Render's free tier limitation) or too many requests are generated. Handle per page at runtime or per request instead.

--- apps/medusa-storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ apps/medusa-storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -1,9 +1,7 @@
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
-import { getCategoryByHandle, listCategories } from '@lib/data/categories';
-import { listRegions } from '@lib/data/regions';
-import { StoreRegion } from '@medusajs/types';
+import { getCategoryByHandle } from '@lib/data/categories';
 import CategoryTemplate from '@modules/categories/templates';
 import { SortOptions } from '@modules/store/components/refinement-list/sort-products';
 
@@ -14,33 +12,6 @@ type Props = {
     page?: string;
   }>;
 };
-
-export async function generateStaticParams() {
-  const product_categories = await listCategories();
-
-  if (!product_categories) {
-    return [];
-  }
-
-  const countryCodes = await listRegions().then((regions: StoreRegion[]) =>
-    regions?.map((r) => r.countries?.map((c) => c.iso_2)).flat()
-  );
-
-  const categoryHandles = product_categories.map(
-    (category: any) => category.handle
-  );
-
-  const staticParams = countryCodes
-    ?.map((countryCode: string | undefined) =>
-      categoryHandles.map((handle: any) => ({
-        countryCode,
-        category: [handle],
-      }))
-    )
-    .flat();
-
-  return staticParams;
-}
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
   const params = await props.params;
