c9b94e7406692769115c9e49434fd17890ba612c
Elloani Ross Pitogo <pelloani@gmail.com>
2025-10-15 18:39:50 +0800

    feat: set up msw mocking in browser

---
+++ apps/medusa-storefront/src/instrumentation-client.ts
@@ -0,0 +1,9 @@
+import('./mocks/msw/browser')
+  .then(async (mod) => {
+    if (mod?.worker?.start) {
+      mod.worker.start({ onUnhandledRequest: 'bypass' });
+    }
+  })
+  .catch(async () => {
+    console.error('Failed to load the browser module');
+  });
