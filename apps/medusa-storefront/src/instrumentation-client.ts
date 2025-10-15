3c404166b610940d1010954fe8962b04677dd18c
Elloani Ross Pitogo <pelloani@gmail.com>
2025-10-15 18:55:15 +0800

    feat: use env variable to disable/enable msw mocks

--- apps/medusa-storefront/src/instrumentation-client.ts
+++ apps/medusa-storefront/src/instrumentation-client.ts
@@ -1,9 +1,11 @@
-import('./mocks/msw/browser')
-  .then(async (mod) => {
-    if (mod?.worker?.start) {
-      mod.worker.start({ onUnhandledRequest: 'bypass' });
-    }
-  })
-  .catch(async () => {
-    console.error('Failed to load the browser module');
-  });
+if (process.env.NEXT_PUBLIC_ENABLE_API_MOCKING === 'true') {
+  import('./mocks/msw/browser')
+    .then(async (mod) => {
+      if (mod?.worker?.start) {
+        mod.worker.start({ onUnhandledRequest: 'bypass' });
+      }
+    })
+    .catch(async () => {
+      console.error('Failed to load the browser module');
+    });
+}
