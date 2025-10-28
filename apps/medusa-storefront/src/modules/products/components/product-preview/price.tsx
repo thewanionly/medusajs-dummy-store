ddf3e4d7b2ea10cf8f22ea9b2a754180f222657d
Elloani Ross Pitogo <pelloani@gmail.com>
2025-10-28 16:08:18 +0800

    refactor: remove async from ProductPreview and PreviewPrice

--- apps/medusa-storefront/src/modules/products/components/product-preview/price.tsx
+++ apps/medusa-storefront/src/modules/products/components/product-preview/price.tsx
@@ -2,7 +2,7 @@ import { VariantPrice } from 'types/global';
 
 import { Text, clx } from '@medusajs/ui';
 
-export default async function PreviewPrice({ price }: { price: VariantPrice }) {
+export default function PreviewPrice({ price }: { price: VariantPrice }) {
   if (!price) {
     return null;
   }
