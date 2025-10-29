d20200ec5e53088b2ffc7134f643afcd8c005a12
Elloani Ross Pitogo <pelloani@gmail.com>
2025-10-29 22:00:24 +0800

    chore: use eslint-plugin-storybook correctly

--- apps/medusa-storefront/eslint.config.mjs
+++ apps/medusa-storefront/eslint.config.mjs
@@ -1,7 +1,7 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import { nextJsConfig } from '@mds/eslint-config/next-js';
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default [...nextJsConfig, ...storybook.configs['flat/recommended']];
