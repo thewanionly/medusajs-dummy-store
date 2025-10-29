a83abc7a2742168af65bccf6f1c056dfe5cee8a6
Elloani Ross Pitogo <pelloani@gmail.com>
2025-10-29 22:14:29 +0800

    feat: add stories files in tailwind config

--- apps/medusa-storefront/tailwind.config.js
+++ apps/medusa-storefront/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/stories/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/modules/**/*.{js,ts,jsx,tsx}',
     './node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}',
