d382819eff65a96b22cf02b63484df47ae2b9bb8
Elloani Ross Pitogo <pelloani@gmail.com>
2025-11-06 12:56:04 +0800

    feat: add cart, customer, and region handlers in index

--- apps/medusa-storefront/src/mocks/msw/handlers/index.ts
+++ apps/medusa-storefront/src/mocks/msw/handlers/index.ts
@@ -1,11 +1,17 @@
+import { handlers as cartHandlers } from './carts';
 import { handlers as collectionsHandlers } from './collections';
+import { handlers as customerHandlers } from './customer';
 import { handlers as productCategoriesHandlers } from './product-categories';
 import { handlers as productsHandlers } from './products';
+import { handlers as regionHandlers } from './regions';
 import { handlers as searchHandlers } from './search';
 
 export const handlers = [
+  ...cartHandlers,
   ...collectionsHandlers,
+  ...customerHandlers,
   ...productCategoriesHandlers,
   ...productsHandlers,
+  ...regionHandlers,
   ...searchHandlers,
 ];
